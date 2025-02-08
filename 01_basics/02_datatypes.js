8f7256a05e1b613e4f79561d3b204661e5d4e00d
Mukul Choudhary <154518817+UnknownUser1901@users.noreply.github.com>
2025-02-08 20:13:05 +0000

    datatypes in javascript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+7) we are using nodejs,not browser
+
+console.log(4
+    +
+    47) //code readability should be high
+
+console.log("Hitesh")
