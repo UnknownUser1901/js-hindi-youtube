d602fda0e4df863ea9d618e561143cc9652bc036
Mukul Choudhary <154518817+UnknownUser1901@users.noreply.github.com>
2025-02-09 07:03:03 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -7,3 +7,22 @@ console.log(4
     47) //code readability should be high
 
 console.log("Hitesh")
+
+let name = "Mukul"
+let age = 23
+let isloggedIn = false
+let state;// undefined
+
+//numbern=> 2 to power 53
+// bigint => used for big number like trading,stockmarket or in large websites like reddit,facebook
+//string=>"" used in single or double quotes
+//boolean=> true/false
+//null => standalone value/representation of an empty value
+//undefined 
+//symbol => used for identifying components i.e uniqueness
+
+//Objects
+
+console.log(typeof isloggedIn);
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
