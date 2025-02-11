4a69678c7aeeca7500ba0af7bc9770d2cbe67410
Mukul Choudhary <154518817+UnknownUser1901@users.noreply.github.com>
2025-02-11 17:13:01 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,19 +4,28 @@
 //let score =  false //0
 //let score = "Mukul" //Nan
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
 
 
 //"33" => 33
 //"33abc" => NaN
 // true => 1; false =>0;
 
-let isLoggedIn = 1
+let isLoggedIn = "Mukul"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//1 =>true; 0=>false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 78
+let stringValue = String(someNumber)
+console.log(typeof stringValue)
+console.log(stringValue)
