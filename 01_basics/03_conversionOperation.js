59fc655666c400bd80bd03646782fdcd7706048a
Mukul Choudhary <154518817+UnknownUser1901@users.noreply.github.com>
2025-02-11 16:54:26 +0000

    datatype conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,22 @@
+//let score = "56dgh"//NaN
+//let score = null //0
+//let score =  undefined//Nan
+//let score =  false //0
+//let score = "Mukul" //Nan
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false =>0;
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
